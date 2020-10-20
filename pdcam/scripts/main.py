f90bfce1785e43215ff118f6366a8129fd87e99d
Jeff McBride <mcbridejc@gmail.com>
2020-10-19 22:19:00 -0700

    Add v4.1 board calibration
    Continue a bad pattern a little longer of hardcoding the board info here...

--- pdcam/scripts/main.py
+++ pdcam/scripts/main.py
@@ -48,6 +48,23 @@ ELECTRODE_LAYOUT_v4 =  [
 ]
 CONTROL_ELECTRODES_v4 = [(0, 2), (0, 7), (7, 1), (7,10), (13, 2), (13, 7)]
 
+ELECTRODE_LAYOUT_v4_1 = [
+    [  1,  2,  3,  4,  5,  6,  7,  8,  9, 10],
+    [ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    [ 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+    [ 31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
+    [ 41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
+    [ 51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+    [ 61, 62, 63, 64, 65, 66, 67, 68, 69, 70],
+    [ 71, 72, 73, 74, 75, 76, 77, 78, 79, 80],
+    [ 81, 82, 83, 84, 85, 86, 87, 88, 89, 90],
+    [ None, None, None, None, 91, 92, None, None, None, None],
+    [ None, None, None, None, 93, 94, None, None, None, None],
+    [ None, None, None, None, 95, 96, None, None, None, None],
+    [ None, None, None, None, 97, 98, None, None, None, None]
+]
+CONTROL_ELECTRODES_v4_1 = [(0, 0), (0, 8), (9, 8), (9, 0)]
+
 ELECTRODE_LAYOUT_v5 = [
     [ None,  7, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,120, None],
     [ None,  1, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,126, None],
@@ -112,8 +129,9 @@ def overlay(reference, imagefile):
 @click.argument('imagefile')
 @click.argument('outfile')
 @click.option('--v4', is_flag=True, default=False)
+@click.option('--v4_1', is_flag=True, default=False)
 @click.option('--v5', is_flag=True, default=False)
-def measure(imagefile, outfile, v4, v5):
+def measure(imagefile, outfile, v4, v4_1, v5):
     img = cv2.cvtColor(cv2.imread(imagefile), cv2.COLOR_BGR2RGB)
 
     fiducials = find_fiducials(img)
@@ -126,6 +144,9 @@ def measure(imagefile, outfile, v4, v5):
     if v4:
         electrode_layout = ELECTRODE_LAYOUT_v4
         control_electrodes = CONTROL_ELECTRODES_v4
+    elif v4_1:
+        electrode_layout = ELECTRODE_LAYOUT_v4_1
+        control_electrodes = CONTROL_ELECTRODES_v4_1
     elif v5: 
         electrode_layout = ELECTRODE_LAYOUT_v5
         control_electrodes = CONTROL_ELECTRODES_v5
@@ -162,7 +183,8 @@ def measure(imagefile, outfile, v4, v5):
     
     cid = fig.canvas.mpl_connect('button_press_event', onclick)
     fig.tight_layout()
-    plt.show(1) 
+    plt.ion()
+    plt.show(block=True)
 
     if len(alignment_points) != len(alignment_electrodes):
         print("Figure closed without collecting enough control points. No file will be saved")
